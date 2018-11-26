b9a42f87bc85572f9a55b8eaf35207d94c78e4f6
= <=>
2018-11-26 17:47:53 -0500

    boton del nav y colores

--- Gemfile
+++ Gemfile
@@ -52,3 +52,4 @@ gem 'tzinfo-data', platforms: [:mingw, :mswin, :x64_mingw, :jruby]
 #Bootstrap-saas
 
 gem 'bootstrap-sass', '~> 3.3', '>= 3.3.7'
+gem 'jquery-rails', '~> 4.3', '>= 4.3.3'
--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -11,7 +11,11 @@
 // about supported directives.
 //
 
-// require rails-ujs
+
+
+
+//= require jquery
+//= require jquery_ujs
 //= require bootstrap
 //= require turbolinks
-= require_tree .
+//= require_tree .
